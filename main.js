10dabd84b4bb94e36fcc9f7840235bce03613a4b
Fabs90 <fabianjuliansyah89@gmail.com>
2023-12-13 14:44:06 +0700

    fix some project description and function to magnify projects img

--- main.js
+++ main.js
@@ -18,3 +18,103 @@ window.addEventListener("scroll", reveal);
 
 // To check the scroll position on page load
 reveal();
+
+// THE CODE BELOW IS FOR IMAGE MAGNIFY
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = "assets/projects-1-full.png";
+  modalImg.alt = this.alt;
+  captionText.innerHTML = this.alt;
+};
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function () {
+  img01.className += " out";
+  setTimeout(function () {
+    modal.style.display = "none";
+    img01.className = "modal-content";
+  }, 400);
+};
+
+var img = document.getElementById("myImg1");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = "assets/projects-2-full.png";
+  modalImg.alt = this.alt;
+  captionText.innerHTML = this.alt;
+};
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function () {
+  img01.className += " out";
+  setTimeout(function () {
+    modal.style.display = "none";
+    img01.className = "modal-content";
+  }, 400);
+};
+
+var img = document.getElementById("myImg2");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = "assets/projects-3-full.png";
+  modalImg.alt = this.alt;
+  captionText.innerHTML = this.alt;
+};
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function () {
+  img01.className += " out";
+  setTimeout(function () {
+    modal.style.display = "none";
+    img01.className = "modal-content";
+  }, 400);
+};
+
+var img = document.getElementById("myImg3");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = "assets/projects-4-full.png";
+  modalImg.alt = this.alt;
+  captionText.innerHTML = this.alt;
+};
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function () {
+  img01.className += " out";
+  setTimeout(function () {
+    modal.style.display = "none";
+    img01.className = "modal-content";
+  }, 400);
+};
+
+var img = document.getElementById("myImg4");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = "assets/projects-5-full.jpeg";
+  modalImg.alt = this.alt;
+  captionText.innerHTML = this.alt;
+};
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function () {
+  img01.className += " out";
+  setTimeout(function () {
+    modal.style.display = "none";
+    img01.className = "modal-content";
+  }, 400);
+};
